36b60caa5810f0fa1560b384bd620e6720182668
lmzjms <85555527+lmzjms@users.noreply.github.com>
2022-05-17 18:56:33 +0800

    Add files via upload

---
+++ const.h
@@ -0,0 +1,17 @@
+/**
+ * @file const.h
+ * @author lmz (3190104724@zju.edu.cn)
+ * @brief 常数定义
+ * @version 0.1
+ * @date 2022-05-17
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef _CONST_H_
+#define _CONST_H_ 1
+
+#define PAGESIZE 4096 /**< 每一页的大小为4KB */
+#define MAXFRAMESIZE 100 /**< 最大页数为100 */
+
+#endif
